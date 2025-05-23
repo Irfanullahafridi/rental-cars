7361733d98127a2ab1f4e7fef89957094cb73c0f
Irfan Ullah Afridi <161113197+Irfanullahafridi@users.noreply.github.com>
2025-05-23 13:05:04 +0500

    Initial commit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,99 +1,99 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Add_User } from "../API/APIUri";
-import { login_User_End } from "../API/EndPoints";
-import axios from "axios";
-import { Eye, EyeOff } from "lucide-react";
+// import { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { Add_User } from "../API/APIUri";
+// import { login_User_End } from "../API/EndPoints";
+// import axios from "axios";
+// import { Eye, EyeOff } from "lucide-react";
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [msg, setMsg] = useState(null);
-  const [showPassword, setShowPassword] = useState(false);
-  const Nav = useNavigate();
+// const Login = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [msg, setMsg] = useState(null);
+//   const [showPassword, setShowPassword] = useState(false);
+//   const Nav = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
 
-    const loginUser = {
-      email: email,
-      password: password,
-    };
-    console.log("login User", loginUser);
-    const login_Url = Add_User + login_User_End;
-    console.log("login URL", login_Url);
-    const res = await axios.post(login_Url, loginUser);
-    console.log("res", res);
-    const data = res.data;
-    if (data) {
-      setMsg(data.msg);
-      localStorage.setItem("Token", data.User_Token);
-      localStorage.setItem("username", data.loginUser.username);
-      localStorage.setItem("userInfo", data.loginUser.email);
-      Nav("/");
-    }
-  };
+//     const loginUser = {
+//       email: email,
+//       password: password,
+//     };
+//     console.log("login User", loginUser);
+//     const login_Url = Add_User + login_User_End;
+//     console.log("login URL", login_Url);
+//     const res = await axios.post(login_Url, loginUser);
+//     console.log("res", res);
+//     const data = res.data;
+//     if (data) {
+//       setMsg(data.msg);
+//       localStorage.setItem("Token", data.User_Token);
+//       localStorage.setItem("username", data.loginUser.username);
+//       localStorage.setItem("userInfo", data.loginUser.email);
+//       Nav("/");
+//     }
+//   };
 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-400 via-purple-500 to-pink-500 px-4 rounded-2xl">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-10 rounded-2xl w-full max-w-md shadow-xl mb-16"
-      >
-        <h2 className="text-3xl font-bold text-center mb-6 text-gray-800 font-serif">
-          Login {msg?.msg}
-        </h2>
+//   return (
+//     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-400 via-purple-500 to-pink-500 px-4 rounded-2xl">
+//       <form
+//         onSubmit={handleSubmit}
+//         className="bg-white p-10 rounded-2xl w-full max-w-md shadow-xl mb-16"
+//       >
+//         <h2 className="text-3xl font-bold text-center mb-6 text-gray-800 font-serif">
+//           Login {msg?.msg}
+//         </h2>
 
-        <label className="block text-sm text-gray-600 mb-1">Email</label>
-        <div className="mb-4">
-          <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-          />
-        </div>
+//         <label className="block text-sm text-gray-600 mb-1">Email</label>
+//         <div className="mb-4">
+//           <input
+//             type="text"
+//             placeholder="Email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             required
+//             className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+//           />
+//         </div>
 
-        <label className="block text-sm text-gray-600 mb-1">Password</label>
-        <div className="mb-4 relative">
-          <input
-            type={showPassword ? "text" : "password"}
-            placeholder="Type your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500 pr-10"
-          />
-          <div
-            className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-500"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-          </div>
-        </div>
+//         <label className="block text-sm text-gray-600 mb-1">Password</label>
+//         <div className="mb-4 relative">
+//           <input
+//             type={showPassword ? "text" : "password"}
+//             placeholder="Type your password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//             required
+//             className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500 pr-10"
+//           />
+//           <div
+//             className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-500"
+//             onClick={() => setShowPassword(!showPassword)}
+//           >
+//             {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+//           </div>
+//         </div>
 
-        <button
-          type="submit"
-          className="cursor-pointer w-full py-2 rounded-full bg-gradient-to-r from-teal-400 to-purple-500 text-white font-semibold text-sm shadow-md hover:from-teal-500 hover:to-purple-600 transition"
-        >
-          LOGIN
-        </button>
+//         <button
+//           type="submit"
+//           className="cursor-pointer w-full py-2 rounded-full bg-gradient-to-r from-teal-400 to-purple-500 text-white font-semibold text-sm shadow-md hover:from-teal-500 hover:to-purple-600 transition"
+//         >
+//           LOGIN
+//         </button>
 
-        <div className="text-center mt-6 text-gray-500 text-sm">Sign Up Using</div>
+//         <div className="text-center mt-6 text-gray-500 text-sm">Sign Up Using</div>
 
-        <div className="text-center mt-1">
-          <Link
-            to="/Register"
-            className="text-sm text-blue-600 hover:underline font-medium"
-          >
-            SIGN UP
-          </Link>
-        </div>
-      </form>
-    </div>
-  );
-};
+//         <div className="text-center mt-1">
+//           <Link
+//             to="/Register"
+//             className="text-sm text-blue-600 hover:underline font-medium"
+//           >
+//             SIGN UP
+//           </Link>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
 
-export default Login;
+// export default Login;
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,141 +1,141 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { FaGoogle } from "react-icons/fa";
-import axios from "axios";
-import { Add_User } from "../API/APIUri";
-import { Add_User_EndPoint } from "../API/EndPoints";
-import { Eye, EyeOff } from "lucide-react";
+// import { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { FaGoogle } from "react-icons/fa";
+// import axios from "axios";
+// import { Add_User } from "../API/APIUri";
+// import { Add_User_EndPoint } from "../API/EndPoints";
+// import { Eye, EyeOff } from "lucide-react";
 
-const Register = () => {
-  const Nav = useNavigate();
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [Msg, setMsg] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+// const Register = () => {
+//   const Nav = useNavigate();
+//   const [username, setUsername] = useState('');
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [confirmPassword, setConfirmPassword] = useState("");
+//   const [Msg, setMsg] = useState("");
+//   const [showPassword, setShowPassword] = useState(false);
+//   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
 
-    if (password !== confirmPassword) {
-      setMsg("Passwords do not match");
-      return;
-    }
+//     if (password !== confirmPassword) {
+//       setMsg("Passwords do not match");
+//       return;
+//     }
 
-    const newUserData = {
-      username: username,
-      email: email,
-      password: password
-    };
-    console.log('new user', newUserData);
-    const UrI = Add_User + Add_User_EndPoint;
-    console.log('UrI', UrI);
-    axios.post(UrI, newUserData)
-      .then((res) => {
-        console.log(res);
-        setMsg(res.data);
-        Nav('/Login');
-      })
-      .catch((error) => { console.log(error) });
-  };
+//     const newUserData = {
+//       username: username,
+//       email: email,
+//       password: password
+//     };
+//     console.log('new user', newUserData);
+//     const UrI = Add_User + Add_User_EndPoint;
+//     console.log('UrI', UrI);
+//     axios.post(UrI, newUserData)
+//       .then((res) => {
+//         console.log(res);
+//         setMsg(res.data);
+//         Nav('/Login');
+//       })
+//       .catch((error) => { console.log(error) });
+//   };
 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-400 via-purple-500 to-pink-500 px-4 rounded-2xl">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white px-10 py-6 rounded-2xl w-full max-w-md shadow-xl"
-      >
-        <h2 className="text-3xl font-bold text-center mb-6 text-gray-800 font-serif">Sign Up {Msg?.Msg}</h2>
+//   return (
+//     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-400 via-purple-500 to-pink-500 px-4 rounded-2xl">
+//       <form
+//         onSubmit={handleSubmit}
+//         className="bg-white px-10 py-6 rounded-2xl w-full max-w-md shadow-xl"
+//       >
+//         <h2 className="text-3xl font-bold text-center mb-6 text-gray-800 font-serif">Sign Up {Msg?.Msg}</h2>
 
-        <div className="mb-4">
-          <label className="block text-sm text-gray-600 mb-1">User Name</label>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-          />
-        </div>
+//         <div className="mb-4">
+//           <label className="block text-sm text-gray-600 mb-1">User Name</label>
+//           <input
+//             type="text"
+//             placeholder="Username"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//             required
+//             className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+//           />
+//         </div>
 
-        <div className="mb-4">
-          <label className="block text-sm text-gray-600 mb-1">Email</label>
-          <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-          />
-        </div>
+//         <div className="mb-4">
+//           <label className="block text-sm text-gray-600 mb-1">Email</label>
+//           <input
+//             type="text"
+//             placeholder="Email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             required
+//             className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+//           />
+//         </div>
 
-        <div className="mb-4 relative">
-          <label className="block text-sm text-gray-600 mb-1">Password</label>
-          <input
-            type={showPassword ? "text" : "password"}
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="w-full px-4 py-2 pr-10 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-          />
-          <div
-            className="absolute inset-y-0 right-3 top-7 flex items-center cursor-pointer text-gray-500"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-          </div>
-        </div>
+//         <div className="mb-4 relative">
+//           <label className="block text-sm text-gray-600 mb-1">Password</label>
+//           <input
+//             type={showPassword ? "text" : "password"}
+//             placeholder="Password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//             required
+//             className="w-full px-4 py-2 pr-10 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+//           />
+//           <div
+//             className="absolute inset-y-0 right-3 top-7 flex items-center cursor-pointer text-gray-500"
+//             onClick={() => setShowPassword(!showPassword)}
+//           >
+//             {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+//           </div>
+//         </div>
 
-        <div className="mb-4 relative">
-          <label className="block text-sm text-gray-600 mb-1">Confirm Password</label>
-          <input
-            type={showConfirmPassword ? "text" : "password"}
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-            className="w-full px-4 py-2 pr-10 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-          />
-          <div
-            className="absolute inset-y-0 right-3 top-7 flex items-center cursor-pointer text-gray-500"
-            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-          >
-            {showConfirmPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-          </div>
-        </div>
+//         <div className="mb-4 relative">
+//           <label className="block text-sm text-gray-600 mb-1">Confirm Password</label>
+//           <input
+//             type={showConfirmPassword ? "text" : "password"}
+//             placeholder="Confirm Password"
+//             value={confirmPassword}
+//             onChange={(e) => setConfirmPassword(e.target.value)}
+//             required
+//             className="w-full px-4 py-2 pr-10 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+//           />
+//           <div
+//             className="absolute inset-y-0 right-3 top-7 flex items-center cursor-pointer text-gray-500"
+//             onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+//           >
+//             {showConfirmPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+//           </div>
+//         </div>
 
-        <button
-          type="submit"
-          className="cursor-pointer w-full py-2 rounded-full bg-gradient-to-r from-teal-400 to-purple-500 text-white font-semibold text-sm shadow-md hover:from-teal-500 hover:to-purple-600 transition"
-        >
-          SIGN UP
-        </button>
+//         <button
+//           type="submit"
+//           className="cursor-pointer w-full py-2 rounded-full bg-gradient-to-r from-teal-400 to-purple-500 text-white font-semibold text-sm shadow-md hover:from-teal-500 hover:to-purple-600 transition"
+//         >
+//           SIGN UP
+//         </button>
 
-        <div className="text-center mt-6 text-gray-500 text-sm">Or Sign Up Using</div>
+//         <div className="text-center mt-6 text-gray-500 text-sm">Or Sign Up Using</div>
 
-        <div className="flex justify-center space-x-4 mt-4">
-          <button
-            type="button"
-            className="cursor-pointer p-2 bg-red-500 hover:bg-red-600 text-white rounded-full"
-          >
-            <FaGoogle />
-          </button>
-        </div>
+//         <div className="flex justify-center space-x-4 mt-4">
+//           <button
+//             type="button"
+//             className="cursor-pointer p-2 bg-red-500 hover:bg-red-600 text-white rounded-full"
+//           >
+//             <FaGoogle />
+//           </button>
+//         </div>
 
-        <p className="text-sm text-center text-gray-600 mt-6">
-          Already have an account?{" "}
-          <Link to="/Login" className="text-blue-600 hover:underline font-medium">
-            Log In
-          </Link>
-        </p>
-      </form>
-    </div>
-  );
-};
+//         <p className="text-sm text-center text-gray-600 mt-6">
+//           Already have an account?{" "}
+//           <Link to="/Login" className="text-blue-600 hover:underline font-medium">
+//             Log In
+//           </Link>
+//         </p>
+//       </form>
+//     </div>
+//   );
+// };
 
-export default Register;
+// export default Register;
